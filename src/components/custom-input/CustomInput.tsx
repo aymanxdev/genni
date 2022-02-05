b795cadcb2bdf74e9d57545d1f6f180d914f91e3
aymanxdev <ayman.bacc@gmail.com>
2022-02-05 21:41:26 +0000

    add invoice inputs as an object

--- src/components/custom-input/CustomInput.tsx
+++ src/components/custom-input/CustomInput.tsx
@@ -1,20 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 
 interface ICustomInputProps {
+  name: string 
   label: string;
   errorMessage: string;
-  onChange: string
+  onChange: React.ChangeEventHandler<HTMLInputElement>
+  id: string,
+  placeholder?: string
 
- 
-  id: string
+
 }
 
-const CustomInput = (props: ICustomInputProps) => {
-  const [focus, setFocus] =
-    useState(false)
+// /** EXAMPLE 
+//      * The input element configuration props.
+//      */
+//  const inputProps: IInputElementProps = {
+//   ...
+//   className: inputClasses.join(' '),
+//   elementConfig: props.elementConfig,
+//   required: props.required || false,
+//   value: state.value,
+//   valid: state.valid,
+//   touched: state.touched,
+//   shouldValidate: state.shouldValidate,
+//   onChangeHandler: onChangeHandler,
+//   style: props.disabled ? { opacity: 0.5, pointerEvents: 'none' } : undefined
+// }
 
-  return <div></div>;
+const CustomInput = (props: ICustomInputProps) => {
+
+  const [focus, setFocus] = useState(false)
+  const {  label, errorMessage, onChange, name,  } = props
+
+const handleFocus = () => {
+  setFocus(true)
+}
+  return <div>
+    <label>{label}</label>
+    <input 
+    
+    onChange={onChange}
+    onBlur={handleFocus}
+    onFocus={()=> name === "title" && setFocus(true)}
+    // focus = {focus.toString()}
+    
+    />
+    <span>{errorMessage}</span>
+  </div>;
 };
 
 export default CustomInput;
