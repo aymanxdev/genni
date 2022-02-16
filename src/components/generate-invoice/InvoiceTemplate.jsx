1f4047980b2cb7a553da97980fc2bf6c1ebe45d7
aymanxdev <ayman.bacc@gmail.com>
2022-02-16 11:52:31 +0000

    modifying webpack for react-pdf library to work

--- src/components/generate-invoice/InvoiceTemplate.jsx
+++ src/components/generate-invoice/InvoiceTemplate.jsx
@@ -13,3 +13,18 @@ const styles = StyleSheet.create({
     flexGrow: 1,
   },
 });
+
+const InvoiceTemplate = () => {
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>;
+};
+
+export default InvoiceTemplate;
